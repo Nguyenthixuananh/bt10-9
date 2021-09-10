d4ade9894cfca3f67b429d195328380465db1d5f
Nguyenthixuananh <nguyenthixuananh09061999@gmail.com>
2021-09-10 15:48:59 +0700

    done10/9

---
+++ THsudungtt.js
@@ -0,0 +1,8 @@
+let inputWidth;
+let inputHeight;
+inputWidth = prompt("Nhập chiều rộng");
+inputHeight = prompt("Nhập chiều dài");
+let width = parseInt(inputWidth);
+let height = parseInt(inputHeight);
+let area = width * height;
+document.write("The area is " + area);
